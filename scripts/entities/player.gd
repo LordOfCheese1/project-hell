98a4f372f37ecf2964f1b3c9062be58e8e4c0614
LordOfCheese <94697288+LordOfCheese1@users.noreply.github.com>
2023-05-15 19:21:18 +0200

    variable jump height
    Don't look at it, it's kinda scuffed

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -1,9 +1,11 @@
 extends "res://scripts/classes/entity_class.gd"
 
 
-var max_speed = 100
+var max_speed = 120
 var accel = 0.15
-var jump_velocity = -245
+var jump_power = 8
+var jump_held_for = 0
+var is_jumping = false
 
 
 func _physics_process(delta):
@@ -12,11 +14,22 @@ func _physics_process(delta):
 	var x_input = Input.get_axis("left", "right")
 	velocity.x = lerp(velocity.x, x_input * max_speed, accel)
 	
+	
 	velocity.y += gravity * delta
 	if is_on_floor():
-		if Input.is_action_pressed("jump"):
+		is_jumping = false
+		if Input.is_action_just_pressed("jump"):
 			jump()
+	else:
+		if is_jumping && jump_held_for < 8 && jump_held_for != 0:
+			if Input.is_action_pressed("jump"):
+				jump_held_for += 1
+			else:
+				jump_held_for = 0
+			velocity.y -= jump_held_for * jump_power
 
 
 func jump():
-	velocity.y = jump_velocity
+	jump_held_for = 2
+	velocity.y = -10
+	is_jumping = true
