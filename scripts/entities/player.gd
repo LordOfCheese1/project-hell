3262740381397d251fd8f92322b8498ab9b3a7f1
LordOfCheese <94697288+LordOfCheese1@users.noreply.github.com>
2023-05-15 19:28:31 +0200

    Jump buffer

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -6,6 +6,8 @@ var accel = 0.15
 var jump_power = 8
 var jump_held_for = 0
 var is_jumping = false
+var jump_buffer = 0
+var max_jump_buffer = 8
 
 
 func _physics_process(delta):
@@ -15,10 +17,14 @@ func _physics_process(delta):
 	velocity.x = lerp(velocity.x, x_input * max_speed, accel)
 	
 	
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer = 8
+	if jump_buffer > 0:
+		jump_buffer -= 1
 	velocity.y += gravity * delta
 	if is_on_floor():
 		is_jumping = false
-		if Input.is_action_just_pressed("jump"):
+		if jump_buffer > 0:
 			jump()
 	else:
 		if is_jumping && jump_held_for < 8 && jump_held_for != 0:
