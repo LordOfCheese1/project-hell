dc70f605eac8ef3bc26c8ddb9f9f1e8ef50dbbae
LordOfCheese <94697288+LordOfCheese1@users.noreply.github.com>
2023-06-09 14:46:57 +0200

    small devil change

--- scripts/entities/enemies/devil.gd
+++ scripts/entities/enemies/devil.gd
@@ -18,3 +18,6 @@ func _physics_process(delta):
 	$parts/upper_arm.rotation_degrees = lerp(prev_rots[1], $parts/upper_arm.rotation_degrees, 0.1)
 	$parts/body.rotation_degrees = velocity.x / 4
 	$parts/upper_arm.position = $parts/lower_arm.position + $parts/lower_arm.transform.x * 17
+	
+	$parts/body/left_jet.rotation_degrees = abs(velocity.x)
+	$parts/body/right_jet.rotation_degrees = -abs(velocity.x)
