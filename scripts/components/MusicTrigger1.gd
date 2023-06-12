546cb8735fedf103555d57daca1853c1173a1a1f
JacobStaffordMusic <jstaffordcomposer@gmail.com>
2023-06-12 11:43:57 -0700

    hellgates music implementation

--- scripts/components/MusicTrigger1.gd
+++ scripts/components/MusicTrigger1.gd
@@ -4,6 +4,10 @@ extends Node2D
 var instance = EventInstance
 var fmodParam1
 var fmodParam2
+var fmodParam3
+var fmodParam4
+var fmodParam5
+
 
 
 func _ready():
@@ -15,15 +19,26 @@ func _ready():
 
 
 #cerberus music dynamic change
-func _on_param_trigger_param_name(paramName1, paramName2):
+func _on_area_2d_param_name(paramName1, paramName2, paramName3, paramName4, paramName5):
 	fmodParam1 = paramName1
 	fmodParam2 = paramName2
+	fmodParam3 = paramName3
+	fmodParam4 = paramName4
+	fmodParam5 = paramName5
 	
-func _on_param_trigger_body_entered(body):
+
+func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
 		instance.set_parameter_by_name_with_label("Intensity", fmodParam1, false)
-		
-func _on_param_trigger_body_exited(body):
+
+func _on_area_2d_body_exited(body):
 	if body.is_in_group("Player"):
 		instance.set_parameter_by_name_with_label("Intensity", fmodParam2, false)
-		
+
+func _on_cerberus_has_died():
+	instance.set_parameter_by_name_with_label("Intensity", fmodParam3, false)
+
+#approch trigger
+func _on_approach_trigger_body_entered(body):
+	if body.is_in_group("Player"):
+		instance.set_parameter_by_name_with_label("Intensity", fmodParam4, false)
--- scripts/components/TriggerArea.gd
+++ scripts/components/TriggerArea.gd
@@ -2,11 +2,15 @@ extends Area2D
 
 @export var paramName1 = ""
 @export var paramName2 = ""
+@export var paramName3 = ""
+@export var paramName4 = ""
+@export var paramName5 = ""
 
-signal param_Name(paramName1, paramName2)
+
+signal param_Name(paramName1, paramName2, paramName3, paramName4, paramName5)
 
 func _ready():
-	emit_signal("param_Name", paramName1, paramName2)
+	emit_signal("param_Name", paramName1, paramName2, paramName3, paramName4, paramName5)
 
  
 
