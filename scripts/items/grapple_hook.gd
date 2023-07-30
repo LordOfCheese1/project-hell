e5b1244549dc1281ced5440741031aec23b98f0d
LordOfCheese <94697288+LordOfCheese1@users.noreply.github.com>
2023-07-31 00:56:51 +0200

    updated grappling hook

--- scripts/items/grapple_hook.gd
+++ scripts/items/grapple_hook.gd
@@ -2,22 +2,34 @@ extends "res://scripts/classes/item_class.gd"
 
 var attached_point = Vector2(0, 0)
 var is_attached = false
+var line_pos = Vector2()
+var velocity = Vector2()
+var touching_wall = false
 
 
 func _ready():
-	setup_item(Vector2(12, 0))
+	setup_item(Vector2(8, 0))
 
 
 func _physics_process(delta):
 	item_update()
 	
+	position += velocity * delta
+	
+	if !touching_wall && grabbed_entity == null && is_attached_to == null:
+		velocity.y += gravity_pull * delta
+		rotation_degrees += velocity.y / 10
+	else:
+		velocity = Vector2(0, 0)
+	
+	velocity.x = lerp(velocity.x, 0.0, 0.01)
+	
 	if grabbed_entity != null:
 		
 		if is_attached:
-			grabbed_entity.velocity.x += Input.get_axis("left", "right") * 10
 			grabbed_entity.velocity.y = clamp(grabbed_entity.velocity.y, -300, 300)
 			$line.set_point_position(1, to_local(attached_point))
-			grabbed_entity.position = lerp(grabbed_entity.position, attached_point, 0.1)
+			grabbed_entity.velocity += (attached_point - grabbed_entity.position) * 0.4
 			$hit_indicator.position = to_local(attached_point)
 		else:
 			if $raycast.is_colliding():
@@ -25,6 +37,12 @@ func _physics_process(delta):
 				$hit_indicator.visible = true
 			else:
 				$hit_indicator.visible = false
+	else:
+		is_attached = false
+		line_pos = to_global(Vector2(0, 0))
+		attached_point = to_global(Vector2(0, 0))
+		$line.set_point_position(1, to_local(attached_point))
+		$hit_indicator.visible = false
 
 
 func _on_used():
@@ -33,8 +51,6 @@ func _on_used():
 			attached_point = $raycast.get_collision_point()
 		else:
 			attached_point = to_global(Vector2(0, 0))
-			if grabbed_entity != null:
-				grabbed_entity.velocity.y = 0
 		is_attached = !is_attached
 		$line.set_point_position(1, to_local(attached_point))
 	else:
@@ -43,3 +59,14 @@ func _on_used():
 			grabbed_entity.velocity.x = grabbed_entity.velocity.x * 2
 		is_attached = false
 		$line.set_point_position(1, to_local(attached_point))
+
+
+func _on_body_entered(body):
+	if body.is_in_group("wall"):
+		position.y -= velocity.y * 0.017
+		touching_wall = true
+
+
+func _on_body_exited(body):
+	if body.is_in_group("wall"):
+		touching_wall = false
--- scripts/world/lava_bubble.gd
+++ scripts/world/lava_bubble.gd
@@ -5,17 +5,16 @@ var did_pop = false
 @export var decay_time = 80
 var textures = [
 	load("res://textures/particles/lavabubble_small.png"),
-	load("res://textures/particles/lavabubble_medium.png"),
 	load("res://textures/particles/lavabubble_big.png")
 ]
 
 
 func _ready():
-	weight = randi_range(2, 4)
+	weight = 4
 	add_to_group("bubble")
 	setup_entity(1.5, 0, 0)
 	$Sprite2D.scale = Vector2(0,0)
-	$Sprite2D.texture = textures[randi_range(0, 2)]
+	$Sprite2D.texture = textures[randi_range(0, 1)]
 
 
 func _physics_process(delta):
